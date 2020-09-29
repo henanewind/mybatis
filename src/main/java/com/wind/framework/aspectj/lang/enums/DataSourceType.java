43089216e1cf3359a821fa65966af7801d7c2519
zjf <zzwonder2009@126.com>
2020-09-29 11:31:23 +0800

    + 增加代码

---
+++ src/main/java/com/wind/framework/aspectj/lang/enums/DataSourceType.java
@@ -0,0 +1,19 @@
+package com.wind.framework.aspectj.lang.enums;
+
+/**
+ * 数据源
+ * 
+ * @author ust
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
